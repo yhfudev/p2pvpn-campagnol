44eb6fde98a42ff014ceb5306b95882c6b25ad39
fbondoux <fbondoux@89029d23-ca48-0410-9ce6-ab42de966641>
2008-04-15 15:45:33 +0000

    Add license informations

--- client/include/log.h
+++ client/include/log.h
@@ -1,3 +1,25 @@
+/*
+ * Small log functions
+ * 
+ * Copyright (C) 2008 Florent Bondoux
+ * 
+ * This file is part of Campagnol.
+ *
+ * Campagnol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Campagnol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Campagnol.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef LOG_H_
 #define LOG_H_
 
