3b758b57ea07fa77908a8ae1c0cfe2c015c60f0c
fbondoux <fbondoux@89029d23-ca48-0410-9ce6-ab42de966641>
2008-04-15 15:45:33 +0000

    Add license informations

--- trunk/client/src/log.c
+++ trunk/client/src/log.c
@@ -1,3 +1,25 @@
+/*
+ * Small log functions
+ * 
+ * Copyright (C) 2008 Florent Bondoux
+ * 
+ * This file is part of Campagnol.
+ *
+ * Campagnol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Campagnol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Campagnol.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <syslog.h>
 #include <stdarg.h>
 
@@ -7,6 +29,10 @@
 
 static int log_enabled;
 
+/*
+ * Initialise syslog if enabled is true
+ * Use name for the logs
+ */
 void log_init(int enabled, const char *name) {
     log_enabled = enabled;
     if (log_enabled) {
@@ -14,10 +40,18 @@ void log_init(int enabled, const char *name) {
     }
 }
 
+/*
+ * Optional
+ * Close syslog
+ */
 void log_close(void) {
     closelog();
 }
 
+/*
+ * Log format/ap if log_enabled is true
+ * Otherwise print the message to stderr if tostderr is true
+ */
 inline void log_message_inner(int tostderr, const char *format, va_list ap) {
     if (log_enabled) {
         vsyslog(LOG_NOTICE, format, ap);
@@ -28,6 +62,9 @@ inline void log_message_inner(int tostderr, const char *format, va_list ap) {
     }
 }
 
+/*
+ * Log a message with systlog or print it to stderr
+ */
 void log_message(const char *format, ...) {
     va_list ap;
     va_start(ap, format);
@@ -35,6 +72,10 @@ void log_message(const char *format, ...) {
     va_end(ap);
 }
 
+/*
+ * Log a message with systlog or print it to stderr
+ * if config.verbose is true
+ */
 void log_message_verb(const char *format, ...) {
     va_list ap;
     va_start(ap, format);
@@ -42,6 +83,10 @@ void log_message_verb(const char *format, ...) {
     va_end(ap);
 }
 
+/*
+ * Print the last error (strerrno) with syslog if log_enabled is true
+ * Call perror otherwise
+ */
 void log_error(const char *s) {
     if (log_enabled) {
         syslog(LOG_NOTICE, "%s: %s", s, strerror(errno));
