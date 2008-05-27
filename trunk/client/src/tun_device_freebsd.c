d326adf86fa0b23ebf12a6971d60f03de3ab66eb
fbondoux <fbondoux@89029d23-ca48-0410-9ce6-ab42de966641>
2008-05-27 14:00:48 +0000

    Add autotools files and remove the old Makefile. Move src/tun_device.c to src/tun_device_linux.c and add the FreeBSD version.

---
+++ trunk/client/src/tun_device_freebsd.c
@@ -0,0 +1,95 @@
+/*
+ * Create and configure a tun device
+ * 
+ * Copyright (C) 2007 Antoine Vianey
+ *               2008 Florent Bondoux
+ * 
+ * This file is part of Campagnol.
+ *
+ * Campagnol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Campagnol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Campagnol.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <net/if_tun.h>
+#include <sys/stat.h>
+
+#include "campagnol.h"
+#include "tun_device.h"
+#include "log.h"
+
+/*
+ * Open a new TUN virtual interface
+ * Bind it to config.vpnIP
+ * istun : use a TUN or TAP device
+ */
+int init_tun(int istun) {
+    int tunfd;
+    struct stat buf;
+    char systemcall[100] = "";          // used to configure the new interface with system shell commands
+    
+    /* Open TUN interface */
+    if (config.verbose) printf("TUN interface initialization\n");
+    if( (tunfd = open("/dev/tun", O_RDWR)) < 0 ) {
+         log_error("Could not open /dev/net/tun");
+         return -1;
+    }
+
+    int i=0;
+    ioctl(tunfd, TUNSLMODE, &i);
+    ioctl(tunfd, TUNSIFHEAD, &i);
+    fstat(tunfd, &buf);
+
+    /* Inteface configuration */
+    if (config.verbose) printf("TUN interface configuration\n");
+    if (config.debug) printf("ifconfig...\n");
+    snprintf(systemcall, 100, "ifconfig %s inet %s %s mtu 1400 up", devname(buf.st_rdev, S_IFCHR), inet_ntoa (config.vpnIP), inet_ntoa(config.vpnIP));
+    if (config.debug) puts(systemcall);
+    system(systemcall);
+    if (config.debug) printf("ip route...\n");
+    snprintf(systemcall, 100, "ip route replace %s via %s", config.network, inet_ntoa (config.vpnIP));
+    if (config.debug) puts(systemcall);
+    int r = system(systemcall);
+    if (r != 0) { // iproute2 is not installed ? try with route
+        if (config.debug) puts("route add...");
+        snprintf(systemcall, 100, "route add -net %s %s", config.network, inet_ntoa (config.vpnIP));
+        if (config.debug) puts(systemcall);
+        r = system(systemcall);
+    }
+    if (r != 0) {
+        log_message_verb("Error while configuring the TUN interface");
+        return -1;
+    }
+        
+    return tunfd;
+}
+
+int close_tun(int fd) {
+    struct stat buf;
+    char systemcall[100] = "";
+    
+    fstat(fd, &buf);
+    close(fd);
+    snprintf(systemcall, 100, "ifconfig %s destroy", devname(buf.st_rdev, S_IFCHR));
+    if (config.debug) puts(systemcall);
+    system(systemcall);
+    return 0;
+}
+
