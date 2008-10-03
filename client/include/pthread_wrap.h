c31fc5285a52754a70ff2eb57be8c7517014f015
fbondoux <fbondoux@89029d23-ca48-0410-9ce6-ab42de966641>
2008-10-03 22:50:29 +0000

    Inline the pthread_wrap functions.

--- client/include/pthread_wrap.h
+++ client/include/pthread_wrap.h
@@ -23,26 +23,199 @@
 #ifndef PTHREAD_WRAP_H_
 #define PTHREAD_WRAP_H_
 
+/*
+ * Wrapper functions around a few pthread functions
+ * These functions exit in case of error
+ */
+
+#include "config.h"
 #include <pthread.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include "log.h"
 
-extern void mutexInit(pthread_mutex_t *mutex, pthread_mutexattr_t *attrs);
-extern void mutexDestroy(pthread_mutex_t *mutex);
-extern void mutexLock(pthread_mutex_t *mutex);
-extern void mutexUnlock(pthread_mutex_t *mutex);
+static inline void mutexInit(pthread_mutex_t *mutex, pthread_mutexattr_t *attrs);
+static inline void mutexDestroy(pthread_mutex_t *mutex);
+static inline void mutexLock(pthread_mutex_t *mutex);
+static inline void mutexUnlock(pthread_mutex_t *mutex);
 
-extern void mutexattrInit(pthread_mutexattr_t *attrs);
-extern void mutexattrDestroy(pthread_mutexattr_t *attrs);
-extern void mutexattrSettype(pthread_mutexattr_t *attrs, int type);
+static inline void mutexattrInit(pthread_mutexattr_t *attrs);
+static inline void mutexattrDestroy(pthread_mutexattr_t *attrs);
+static inline void mutexattrSettype(pthread_mutexattr_t *attrs, int type);
 
-extern void conditionInit(pthread_cond_t *cond, pthread_condattr_t *attrs);
-extern void conditionDestroy(pthread_cond_t *cond);
-extern int conditionWait(pthread_cond_t *cond, pthread_mutex_t *mutex);
-extern int conditionTimedwait(pthread_cond_t *cond, pthread_mutex_t *mutex, const struct timespec *abs_timeout);
-extern int conditionBroadcast(pthread_cond_t *cond);
-extern int conditionSignal(pthread_cond_t *cond);
+static inline void conditionInit(pthread_cond_t *cond, pthread_condattr_t *attrs);
+static inline void conditionDestroy(pthread_cond_t *cond);
+static inline int conditionWait(pthread_cond_t *cond, pthread_mutex_t *mutex);
+static inline int conditionTimedwait(pthread_cond_t *cond, pthread_mutex_t *mutex, const struct timespec *abs_timeout);
+static inline int conditionBroadcast(pthread_cond_t *cond);
+static inline int conditionSignal(pthread_cond_t *cond);
 
-extern pthread_t createThread(void * (*start_routine)(void *), void * arg);
-extern pthread_t createDetachedThread(void * (*start_routine)(void *), void * arg);
-extern void joinThread(pthread_t thread, void **value_ptr);
+static inline pthread_t createThread(void * (*start_routine)(void *), void * arg);
+static inline pthread_t createDetachedThread(void * (*start_routine)(void *), void * arg);
+static inline void joinThread(pthread_t thread, void **value_ptr);
+
+
+void mutexInit(pthread_mutex_t *mutex, pthread_mutexattr_t *attrs) {
+    ASSERT(mutex);
+    int r = pthread_mutex_init(mutex, attrs);
+    if (r != 0) {
+        log_message("Error pthread_mutex_init(): %s", strerror(r));
+        exit(EXIT_FAILURE);
+    }
+}
+
+void mutexDestroy(pthread_mutex_t *mutex) {
+    ASSERT(mutex);
+    int r = pthread_mutex_destroy(mutex);
+    if (r != 0) {
+        log_message("Error pthread_mutex_destroy(): %s", strerror(r));
+        exit(EXIT_FAILURE);
+    }
+}
+
+void mutexLock(pthread_mutex_t *mutex) {
+    ASSERT(mutex);
+    int r = pthread_mutex_lock(mutex);
+    if (r != 0) {
+        log_message("Error pthread_mutex_lock(): %s", strerror(r));
+        exit(EXIT_FAILURE);
+    }
+}
+
+void mutexUnlock(pthread_mutex_t *mutex) {
+    ASSERT(mutex);
+    int r = pthread_mutex_unlock(mutex);
+    if (r != 0) {
+        log_message("Error pthread_mutex_unlock(): %s", strerror(r));
+        exit(EXIT_FAILURE);
+    }
+}
+
+void mutexattrInit(pthread_mutexattr_t *attrs) {
+    ASSERT(attrs);
+    int r = pthread_mutexattr_init(attrs);
+    if (r != 0) {
+        log_message("Error pthread_mutexattr_init(): %s", strerror(r));
+        exit(EXIT_FAILURE);
+    }
+}
+
+void mutexattrSettype(pthread_mutexattr_t *attrs, int type) {
+    ASSERT(attrs);
+    int r = pthread_mutexattr_settype(attrs, type);
+    if (r != 0) {
+        log_message("Error pthread_mutexattr_settype(): %s", strerror(r));
+        exit(EXIT_FAILURE);
+    }
+}
+
+void mutexattrDestroy(pthread_mutexattr_t *attrs) {
+    ASSERT(attrs);
+    int r = pthread_mutexattr_destroy(attrs);
+    if (r != 0) {
+        log_message("Error pthread_mutexattr_destroy(): %s", strerror(r));
+        exit(EXIT_FAILURE);
+    }
+}
+
+void conditionInit(pthread_cond_t *cond, pthread_condattr_t *attrs) {
+    ASSERT(cond);
+    int r = pthread_cond_init(cond, attrs);
+    if (r != 0) {
+        log_message("Error pthread_cond_init(): %s", strerror(r));
+        exit(EXIT_FAILURE);
+    }
+}
+
+void conditionDestroy(pthread_cond_t *cond) {
+    ASSERT(cond);
+    int r = pthread_cond_destroy(cond);
+    if (r != 0) {
+        log_message("Error pthread_cond_destroy(): %s", strerror(r));
+        exit(EXIT_FAILURE);
+    }
+}
+
+int conditionWait(pthread_cond_t *cond, pthread_mutex_t *mutex) {
+    ASSERT(cond);
+    int retval;
+    retval = pthread_cond_wait(cond, mutex);
+    if (retval != 0) {
+        log_message("Error pthread_cond_wait(): %s", strerror(retval));
+        exit(EXIT_FAILURE);
+    }
+    return retval;
+}
+
+int conditionTimedwait(pthread_cond_t *cond, pthread_mutex_t *mutex,
+        const struct timespec *abs_timeout) {
+    ASSERT(cond);
+    ASSERT(abs_timeout);
+    int retval;
+    retval = pthread_cond_timedwait(cond, mutex, abs_timeout);
+    if (retval != 0 && retval != ETIMEDOUT) {
+        log_message("Error pthread_cond_timedwait(): %s", strerror(retval));
+        exit(EXIT_FAILURE);
+    }
+    return retval;
+}
+
+int conditionBroadcast(pthread_cond_t *cond) {
+    ASSERT(cond);
+    int retval;
+    retval = pthread_cond_broadcast(cond);
+    if (retval != 0) {
+        log_message("Error pthread_cond_broadcast(): %s", strerror(retval));
+        exit(EXIT_FAILURE);
+    }
+    return retval;
+}
+
+int conditionSignal(pthread_cond_t *cond) {
+    ASSERT(cond);
+    int retval;
+    retval = pthread_cond_signal(cond);
+    if (retval != 0) {
+        log_message("Error pthread_cond_signal(): %s", strerror(retval));
+        exit(EXIT_FAILURE);
+    }
+    return retval;
+}
+
+/*
+ * Create a thread executing start_routine with the arguments arg
+ * without attributes
+ */
+pthread_t createThread(void * (*start_routine)(void *), void * arg) {
+    ASSERT(start_routine);
+    pthread_t thread;
+    if (pthread_create(&thread, NULL, start_routine, arg) != 0) {
+        log_error("Error pthread_create()");
+        exit(EXIT_FAILURE);
+    }
+    return thread;
+}
+
+/*
+ * Create a thread executing start_routine with the arguments arg
+ * without attributes. Then call pthread_detach.
+ */
+pthread_t createDetachedThread(void * (*start_routine)(void *), void * arg) {
+    pthread_t thread = createThread(start_routine, arg);
+    if (pthread_detach(thread) != 0) {
+        log_error("Error pthread_detach()");
+        exit(EXIT_FAILURE);
+    }
+    return thread;
+}
+
+void joinThread(pthread_t thread, void **value_ptr) {
+    int r = pthread_join(thread, value_ptr);
+    if (r != 0) {
+        log_message("Error pthread_join(): %s", strerror(r));
+        exit(EXIT_FAILURE);
+    }
+}
 
 #endif /*PTHREAD_WRAP_H_*/
