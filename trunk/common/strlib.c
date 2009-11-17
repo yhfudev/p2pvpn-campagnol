b0e98f5014c8e96ab4d646ed2b67e2846461922e
fbondoux <fbondoux@89029d23-ca48-0410-9ce6-ab42de966641>
2009-11-17 22:43:05 +0000

    Add a simple string library to ease string manipulation in the parser.

---
+++ trunk/common/strlib.c
@@ -0,0 +1,144 @@
+/*
+ * This file is part of Campagnol VPN.
+ * Copyright (C) 2009  Florent Bondoux
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include "log.h"
+#include "strlib.h"
+
+/*
+ * Init a string buffer.
+ * The buffer is valid and nul-terminated
+ */
+void strlib_init(strlib_buf_t *sb) {
+    sb->len = 0;
+    sb->buflen = 0;
+    sb->mark = 0;
+    sb->s = NULL;
+    strlib_grow(sb, 1);
+    sb->s[0] = '\0';
+}
+
+/*
+ * Free the buffer
+ */
+void strlib_free(strlib_buf_t *sb) {
+    free(sb->s);
+    sb->s = NULL;
+    sb->len = 0;
+    sb->buflen = 0;
+}
+
+/*
+ * Erase the string
+ */
+void strlib_reset(strlib_buf_t *sb) {
+    sb->len = 0;
+    sb->mark = 0;
+    sb->s[sb->len] = '\0';
+}
+
+/*
+ * Set the mark for strlib_rstrip
+ */
+void strlib_setmark(strlib_buf_t *sb, size_t mark) {
+    sb->mark = mark;
+}
+
+/*
+ * Grow the buffer so that it's possible to append a string of length n
+ */
+void strlib_grow(strlib_buf_t *sb, size_t n) {
+    size_t buflen_new;
+    size_t buflen_grow;
+    buflen_new = sb->len + n + 1;
+    if (buflen_new > sb->buflen) {
+        // This growing factor is from git's cache.h
+        buflen_grow = (sb->buflen + 16) * 3 / 2;
+        if (buflen_grow < buflen_new)
+            sb->buflen = buflen_new;
+        else
+            sb->buflen = buflen_grow;
+        sb->s = CHECK_ALLOC_FATAL(realloc(sb->s, sb->buflen));
+    }
+}
+
+/*
+ * Append a single char
+ */
+void strlib_push(strlib_buf_t *sb, char c) {
+    strlib_grow(sb, 1);
+    sb->s[sb->len] = c;
+    sb->len++;
+    sb->s[sb->len] = '\0';
+}
+
+/*
+ * Append a string of length len
+ */
+void strlib_append(strlib_buf_t *sb, const char *s, size_t len) {
+    strlib_grow(sb, len);
+    memcpy(sb->s + sb->len, s, len);
+    sb->len += len;
+    sb->s[sb->len] = '\0';
+}
+
+/*
+ * Remove the trailing blank (space or tab) characters.
+ * Stop at the first non blank char or at the mark
+ */
+void strlib_rstrip(strlib_buf_t *sb) {
+    while (sb->len > 0 && sb->len > sb->mark && isblank(sb->s[sb->len - 1]))
+        sb->len--;
+    sb->s[sb->len] = '\0';
+}
+
+/*
+ * Append src to dest
+ */
+void strlib_appendbuf(strlib_buf_t *dest, strlib_buf_t *src) {
+    strlib_append(dest, src->s, src->len);
+}
+
+/* Append a formated string to the buffer */
+void strlib_appendf(strlib_buf_t *sb, const char *fmt, ...) {
+    va_list ap;
+    int len;
+
+    va_start(ap, fmt);
+    len = vsnprintf(sb->s + sb->len, sb->buflen - sb->len, fmt, ap);
+    va_end(ap);
+    if (len < 0) {
+        log_error(errno, "vsnprintf");
+        exit(EXIT_FAILURE);
+    }
+    if (len >= (int) (sb->buflen - sb->len)) {
+        strlib_grow(sb, len);
+        va_start(ap, fmt);
+        len = vsnprintf(sb->s + sb->len, sb->buflen - sb->len, fmt, ap);
+        va_end(ap);
+    }
+    sb->len += len;
+}
