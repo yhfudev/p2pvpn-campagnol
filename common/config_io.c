b6083d4ca733779030613e9020c4e4e1e90e0861
fbondoux <fbondoux@89029d23-ca48-0410-9ce6-ab42de966641>
2009-11-18 22:26:18 +0000

    Add support for the \ooo and \hxx escape sequences. Correct the handling of OEF.

--- common/config_io.c
+++ common/config_io.c
@@ -27,16 +27,6 @@
 #include "config_parser.h"
 #include "log.h"
 
-/* Reading structure with a 1 byte readahead */
-struct read_buf {
-    int next; //next char
-    FILE *f; // stream
-};
-typedef struct read_buf read_buf_t;
-
-static void read_buf_init(read_buf_t *rb, FILE *f);
-static inline int read_buf_next(read_buf_t *rb);
-
 /* The position in the file */
 struct position {
     size_t line; // line
@@ -44,17 +34,35 @@ struct position {
 };
 typedef struct position position_t;
 
+/*
+ * Reading structure with a 1 byte readahead
+ * The readahead is used to transform \r\n into \n while reading
+ * and to handle the octal and hexadecimal escape sequences
+ */
+struct read_buf {
+    int next; //next char
+    position_t *pos; // Current position
+    FILE *f; // stream
+};
+typedef struct read_buf read_buf_t;
+
+static void read_buf_init(read_buf_t *rb, position_t *pos, FILE *f);
+static inline int read_buf_next(read_buf_t *rb);
+static inline int read_buf_peek(read_buf_t *rb);
+
 static void position_init(position_t *pos);
 static inline void position_inc_line(position_t *pos);
 static inline void position_inc_char(position_t *pos);
 
-void read_buf_init(read_buf_t *rb, FILE *f) {
+void read_buf_init(read_buf_t *rb, position_t *pos, FILE *f) {
     rb->f = f;
+    rb->pos = pos;
     rb->next = fgetc(f);
 }
 
 /*
  * Read the next char and convert \r\n sequences to \n
+ * Update the position structure
  */
 int read_buf_next(read_buf_t *rb) {
     int c;
@@ -65,8 +73,19 @@ int read_buf_next(read_buf_t *rb) {
             rb->next = fgetc(rb->f);
             c = '\n';
         }
+        if (c == '\n')
+            position_inc_line(rb->pos);
+        else
+            position_inc_char(rb->pos);
     }
     return c;
+}
+
+/*
+ * Get the next character on the stream without removing that char
+ */
+int read_buf_peek(read_buf_t *rb) {
+    return rb->next;
 }
 
 void position_init(position_t *pos) {
@@ -84,15 +103,38 @@ void position_inc_char(position_t *pos) {
 }
 
 /*
- * Test for # or ;
+ * Convert an hexadecimal digit to its int value.
+ * Assume that isxdigit(c) == true
  */
-static inline int test_comment(int c) {
-    return c == '#' || c == ';';
+static inline int hextoint(char c) {
+    if (isdigit(c))
+        return c - '0';
+    else if (islower(c))
+        return c - 'a' + 10;
+    else
+        return c - 'A' + 10;
 }
 
-static inline void char_unescape(strlib_buf_t *sb, int c) {
+/* checks for octal digits */
+static inline int isoctdigit(int c) {
+    return c >= '0' && c <= '7';
+}
+
+/*
+ * Convert an octal digit to its int value.
+ * Assume that isoctdigit(c) == true
+ */
+static inline int octtoint(char c) {
+    return c - '0';
+}
+
+/*
+ * Interprete an escaped char c, write the output into sb.
+ * Support the C escape sequences and \[ \] \; \# \=
+ */
+static inline void char_unescape(strlib_buf_t *sb, int c, read_buf_t *rb) {
+    int v;
     switch (c) {
-        // C strings, except \nnn and \xhh
         case '\\':
         case '\"':
         case '\'':
@@ -120,7 +162,26 @@ static inline void char_unescape(strlib_buf_t *sb, int c) {
         case 'v':
             strlib_push(sb, '\v');
             break;
-            // Additional escape sequences
+        case 'x':
+            // C hexadecimal value, read up to 2 digits
+            c = read_buf_peek(rb);
+            if (isxdigit(c)) {
+                v = hextoint(c);
+                read_buf_next(rb);
+                c = read_buf_peek(rb);
+                if (isxdigit(c)) {
+                    v <<= 4;
+                    v += hextoint(c);
+                    read_buf_next(rb);
+                }
+                strlib_push(sb, (char) v);
+            }
+            else {
+                strlib_push(sb, '\\');
+                strlib_push(sb, 'x');
+                break;
+            }
+            break;
         case '#':
         case ';':
         case '[':
@@ -129,15 +190,36 @@ static inline void char_unescape(strlib_buf_t *sb, int c) {
             strlib_push(sb, (char) c);
             break;
         default:
-            strlib_push(sb, '\\');
-            strlib_push(sb, (char) c);
+            // C octal value, read up to 3 digits
+            if (isoctdigit(c)) {
+                v = octtoint(c);
+                c = read_buf_peek(rb);
+                if (isoctdigit(c)) {
+                    read_buf_next(rb);
+                    v <<= 3;
+                    v += octtoint(c);
+                    c = read_buf_peek(rb);
+                    if (isoctdigit(c)) {
+                        read_buf_next(rb);
+                        v <<= 3;
+                        v += octtoint(c);
+                    }
+                }
+                strlib_push(sb, (char) v);
+            }
+            else {
+                strlib_push(sb, '\\');
+                strlib_push(sb, (char) c);
+            }
             break;
     }
 }
 
+/*
+ * Write c into sb, escaping the char if necessary
+ */
 static inline void char_escape(strlib_buf_t *sb, char c) {
     switch (c) {
-        // C strings, except \nnn and \xhh
         case '\\':
         case '\"':
             //        case '\'':
@@ -165,7 +247,6 @@ static inline void char_escape(strlib_buf_t *sb, char c) {
         case '\v':
             strlib_append(sb, "\\v", 2);
             break;
-            // Additional escape sequences
         case '#':
         case ';':
         case '[':
@@ -202,26 +283,27 @@ static int nparser_get_next_idt(read_buf_t *rb, const char *name,
         ST_IDT_COMMENT, // The line contains a comment
         ST_IDT_AFTER_COMMENT, // Comment after an identifier
         ST_IDT_SKIP, // Skip until the end of the line
-        ST_IDT_END
-    // Stop reading
+        ST_IDT_END // Stop reading
     } state = ST_IDT_START;
     int c;
-    int eol;
+    int eol, eof, comment;
 
     strlib_reset(sb);
 
     while (state != ST_IDT_END) {
         c = read_buf_next(rb);
-        if (c == EOF)
-            break;
 
+        eof = (c == EOF);
         eol = (c == '\n');
+        comment = (c == '#' || c == ';');
 
         switch (state) {
             case ST_IDT_START:
-                if (c == '[')
+                if (eof)
+                    state = ST_IDT_END;
+                else if (c == '[')
                     state = ST_IDT_BEGIN_SECTION;
-                else if (test_comment(c)) {
+                else if (comment) {
                     state = ST_IDT_COMMENT;
                 }
                 else if (c == ']') {
@@ -243,14 +325,14 @@ static int nparser_get_next_idt(read_buf_t *rb, const char *name,
                 }
                 break;
             case ST_IDT_BEGIN_SECTION:
-                if (eol) {
+                if (eol || eof) {
                     log_message(
                             "[%s:%zu:%zu] Syntax error: section declaration",
                             name, pos->line, pos->c);
                     state = ST_IDT_END;
                     r = -1;
                 }
-                else if (test_comment(c)) {
+                else if (comment) {
                     log_message(
                             "[%s:%zu:%zu] Syntax error: section declaration",
                             name, pos->line, pos->c);
@@ -269,7 +351,7 @@ static int nparser_get_next_idt(read_buf_t *rb, const char *name,
                 }
                 break;
             case ST_IDT_READ_SECTION:
-                if (eol) {
+                if (eol || eof) {
                     log_message(
                             "[%s:%zu:%zu] Syntax error: section declaration",
                             name, pos->line, pos->c);
@@ -279,7 +361,7 @@ static int nparser_get_next_idt(read_buf_t *rb, const char *name,
                 else if (c == '\\') {
                     state = ST_IDT_READ_SECTION_ESC;
                 }
-                else if (test_comment(c)) {
+                else if (comment) {
                     log_message(
                             "[%s:%zu:%zu] Syntax error: section declaration",
                             name, pos->line, pos->c);
@@ -295,7 +377,7 @@ static int nparser_get_next_idt(read_buf_t *rb, const char *name,
                 }
                 break;
             case ST_IDT_READ_SECTION_ESC:
-                if (eol) {
+                if (eol || eof) {
                     log_message(
                             "[%s:%zu:%zu] Syntax error: line continuation not allowed in identifiers",
                             name, pos->line, pos->c);
@@ -303,15 +385,15 @@ static int nparser_get_next_idt(read_buf_t *rb, const char *name,
                     r = -1;
                 }
                 else {
-                    char_unescape(sb, c);
+                    char_unescape(sb, c, rb);
                     state = ST_IDT_READ_SECTION;
                 }
                 break;
             case ST_IDT_AFTER_SECTION:
-                if (eol) {
+                if (eol || eof) {
                     state = ST_IDT_END;
                 }
-                else if (test_comment(c)) {
+                else if (comment) {
                     state = ST_IDT_AFTER_COMMENT;
                 }
                 else if (!isblank(c)) {
@@ -322,7 +404,7 @@ static int nparser_get_next_idt(read_buf_t *rb, const char *name,
                 }
                 break;
             case ST_IDT_READ_NAME:
-                if (eol) {
+                if (eol || eof) {
                     log_message("[%s:%zu:%zu] Syntax error: no value", name,
                             pos->line, pos->c);
                     state = ST_IDT_END;
@@ -331,7 +413,7 @@ static int nparser_get_next_idt(read_buf_t *rb, const char *name,
                 else if (c == '\\') {
                     state = ST_IDT_READ_NAME_ESC;
                 }
-                else if (test_comment(c)) {
+                else if (comment) {
                     log_message("[%s:%zu:%zu] Syntax error: no value", name,
                             pos->line, pos->c);
                     state = ST_IDT_SKIP;
@@ -346,7 +428,7 @@ static int nparser_get_next_idt(read_buf_t *rb, const char *name,
                 }
                 break;
             case ST_IDT_READ_NAME_ESC:
-                if (eol) {
+                if (eol || eof) {
                     log_message(
                             "[%s:%zu:%zu] Syntax error: line continuation not allowed in identifiers",
                             name, pos->line, pos->c);
@@ -354,7 +436,7 @@ static int nparser_get_next_idt(read_buf_t *rb, const char *name,
                     r = -1;
                 }
                 else {
-                    char_unescape(sb, c);
+                    char_unescape(sb, c, rb);
                     state = ST_IDT_READ_NAME;
                 }
                 break;
@@ -362,25 +444,23 @@ static int nparser_get_next_idt(read_buf_t *rb, const char *name,
                 if (eol) {
                     state = ST_IDT_START;
                 }
+                else if (eof) {
+                    state = ST_IDT_END;
+                }
                 break;
             case ST_IDT_AFTER_COMMENT:
-                if (eol) {
+                if (eol || eof) {
                     state = ST_IDT_END;
                 }
                 break;
             case ST_IDT_SKIP:
-                if (eol) {
+                if (eol || eof) {
                     state = ST_IDT_END;
                 }
                 break;
             case ST_IDT_END:
                 break;
         }
-
-        if (eol)
-            position_inc_line(pos);
-        else
-            position_inc_char(pos);
     }
     if (r == 1 || r == 2) {
         strlib_rstrip(sb);
@@ -391,7 +471,7 @@ static int nparser_get_next_idt(read_buf_t *rb, const char *name,
 
 /*
  * Read the next value
- * Return -1 on error, 0 on success
+ * Return -1 on error (EOF), 0 on success
  * The value is copied into sb and the position in the stream is updated
  * The parser stops after the final end of line.
  */
@@ -404,28 +484,27 @@ static int nparser_get_next_value(read_buf_t *rb, const char *name,
         ST_VAL_IN_UNQUOTED, // Read an unquoted value
         ST_VAL_IN_UNQUOTED_ESC, // Escaped char in an unquoted value
         ST_VAL_COMMENT, // Read a comment
-        ST_VAL_SKIP, // Skip until the end of the line
-        ST_VAL_END
-    // Stop reading
+        ST_VAL_END // Stop reading
     } state = ST_VAL_START;
     int c;
-    int eol;
+    int r = 0;
+    int eol, eof, comment;
 
     strlib_reset(sb);
 
     while (state != ST_VAL_END) {
         c = read_buf_next(rb);
-        if (c == EOF)
-            break;
 
+        eof = (c == EOF);
         eol = (c == '\n');
+        comment = (c == '#' || c == ';');
 
         switch (state) {
             case ST_VAL_START:
-                if (eol) {
+                if (eol || eof) {
                     state = ST_VAL_END;
                 }
-                else if (test_comment(c)) {
+                else if (comment) {
                     state = ST_VAL_COMMENT;
                 }
                 else if (isblank(c)) {
@@ -442,7 +521,11 @@ static int nparser_get_next_value(read_buf_t *rb, const char *name,
                 }
                 break;
             case ST_VAL_IN_QUOTE:
-                if (eol) {
+                if (eof) {
+                    state = ST_VAL_END;
+                    r = -1;
+                }
+                else if (eol) {
                     strlib_push(sb, '\n');
                 }
                 else if (c == '\"') {
@@ -457,13 +540,19 @@ static int nparser_get_next_value(read_buf_t *rb, const char *name,
                 }
                 break;
             case ST_VAL_IN_QUOTE_ESC:
-                if (!eol) {
-                    char_unescape(sb, c);
+                if (eof) {
+                    state = ST_VAL_END;
+                    r = -1;
                 }
-                state = ST_VAL_IN_QUOTE;
+                else {
+                    if (!eol) {
+                        char_unescape(sb, c, rb);
+                    }
+                    state = ST_VAL_IN_QUOTE;
+                }
                 break;
             case ST_VAL_IN_UNQUOTED:
-                if (eol) {
+                if (eol || eof) {
                     strlib_rstrip(sb);
                     state = ST_VAL_END;
                 }
@@ -479,44 +568,34 @@ static int nparser_get_next_value(read_buf_t *rb, const char *name,
                 }
                 break;
             case ST_VAL_IN_UNQUOTED_ESC:
-                if (eol) {
+                if (eof) {
+                    state = ST_VAL_END;
+                    r = -1;
+                }
+                else if (eol) {
                     strlib_rstrip(sb);
                     state = ST_VAL_START;
                 }
                 else {
-                    char_unescape(sb, c);
+                    char_unescape(sb, c, rb);
                     state = ST_VAL_IN_UNQUOTED;
                 }
                 break;
             case ST_VAL_COMMENT:
-                if (eol) {
-                    state = ST_VAL_END;
-                }
-                break;
-            case ST_VAL_SKIP:
-                if (eol) {
+                if (eol || eof) {
                     state = ST_VAL_END;
                 }
                 break;
             case ST_VAL_END:
                 break;
         }
-
-        if (eol)
-            position_inc_line(pos);
-        else
-            position_inc_char(pos);
     }
 
-    if (state != ST_VAL_IN_QUOTE && state != ST_VAL_IN_QUOTE_ESC && state
-            != ST_VAL_IN_UNQUOTED_ESC) {
-        return 0;
-    }
-    else {
+    if (r == -1) {
         log_message("[%s:%zu:%zu] Syntax error: unexpected end of file", name,
                 pos->line, pos->c);
-        return -1;
     }
+    return r;
 }
 
 /*
@@ -537,8 +616,8 @@ void parser_read(const char *confFile, parser_context_t *parser, int debug) {
         exit(EXIT_FAILURE);
     }
 
-    read_buf_init(&rb, conf);
     position_init(&pos);
+    read_buf_init(&rb, &pos, conf);
     strlib_init(&sb_idt);
     strlib_init(&sb_val);
     strlib_init(&sb_section);
